387271536843d86c77f6e3fdf20f7f256d5812d0
gabrielfeloiu-creator <gabriel.feloiu@gmail.com>
2026-06-01 16:59:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,4 @@ def get_players():
     players = prepare_player_data(df)
     return jsonify(players)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
